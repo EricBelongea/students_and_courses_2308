57b234e0466fdc48756afb0514c2426ff35088bd
Eric Belongea <eric.belongea@gmail.com>
2023-09-06 09:26:40 -0600

    Adds student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
-  
+  attr_reader :name, :age, :scores
+
+  def initialize(person)
+    @name = person[:name]
+    @age = person[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum(0.0) / @scores.size
+  end
 end
